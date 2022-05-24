bff6a03578f8cabd9570c3d49a27b6b1c944bff8
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-05-24 17:17:29 +0200

    end

--- database.php
+++ database.php
@@ -45,9 +45,9 @@ $domande = [
         "domanda"=> "Stabilire il paese associato al tuo account",
         "risposta"=>"Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nell'ultimo anno.<br> <br>
 
-        I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.<br> <br>
+        I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l'associazione del paese.<br><br>
         
-        Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.<br> <br>"
+        Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, l'installazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.<br><br>" 
     ],
     [
         "domanda"=> "Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?",
--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php    
 include 'database.php';
+//var_dump($domande)
 
 
 
@@ -12,14 +13,31 @@ include 'database.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=, initial-scale=1.0">
     <link rel="stylesheet" href="./css/style.css">
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
     <title>Google faq</title>
 </head>
 <body>
     <nav>
-
+        <div>
+            <i class="fa-solid fa-bars"></i>
+            <h2>Domande frequenti</h2>
+        </div>
+        <div>
+        <i class="fa-solid fa-table-cells"></i>
+        <i class="fa-solid fa-user"></i>
+        </div>
     </nav>
     <main>
+        <div>
+            <?php foreach($domande as $key => $value) {
 
+                echo " <h4> " . $value["domanda"] . "</h4> ";
+
+                echo "<p> " . $value["risposta"] . " </p> ";
+               
+
+            } ?>
+         </div>
     </main>
     
 </body>
